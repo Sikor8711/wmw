9b93b4c1cdcafe8bc2e92259ec9d2945417c4070
Patryk Sikorski <sikor87@hotmail.com>
2026-03-20 11:20:32 +0000

    setings

--- src/components/paymant.rs
+++ src/components/paymant.rs
@@ -2,6 +2,8 @@ use leptos::prelude::*;
 
 #[component]
 pub fn CheckoutPage() -> impl IntoView {
+    let (email, set_email) = signal("".to_string());
+
     let token = "test_03ea1e075f335d2b2f3cdf22b4c";
     let price_id = "pri_01kgw2nvx9mf3a3d7neqz9jpkn";
     let price_id2 = "pri_01kkmp3eedgkwphde7w3wrjvs7";
@@ -9,43 +11,49 @@ pub fn CheckoutPage() -> impl IntoView {
     let script = format!(
         r#"
 Paddle.Environment.set('sandbox');
-Paddle.Initialize({{ token: '{}' }});
+Paddle.Initialize({{ token: '{token}' }});
 Paddle.Checkout.open({{
 settings: {{
+variant: 'one-page',
 displayMode: 'inline',
 frameTarget: 'paddle-checkout-box',
-frameInitialHeight: 700,
+frameInitialHeight: 450,
 frameStyle: 'width: 100%; background-color: transparent; border: none;'
 }},
-items: [{{ priceId: '{}', quantity: 1 }}]
+items: [{{ priceId: '{price_id}', quantity: 1 }}]
+customer: {{email: '{:?email}'}},
 }});
 
 function toggleItem(checkbox) {{
 if (checkbox.checked) {{
 Paddle.Checkout.updateCheckout({{
 items: [
-{{ priceId: '{}', quantity: 1 }},
-{{ priceId: '{}', quantity: 1 }}
+{{ priceId: '{price_id}', quantity: 1 }},
+{{ priceId: '{price_id2}', quantity: 1 }}
 ]
 }});
 }} else {{
 Paddle.Checkout.updateCheckout({{
-items: [{{ priceId: '{}', quantity: 1 }}]
+items: [{{ priceId: '{price_id}', quantity: 1 }}]
 }});
 }}
 }}
-"#,
-        token, price_id, price_id, price_id2, price_id
+"#
     );
 
     view! {
     <div>
-        <label>
+    </div>
+
+    <div class="sc-DWqcv kLCWFP min-h-[450]">
+
+        <label id="check">
             <input type="checkbox" onclick="toggleItem(this)" />
             "Add coaching session"
         </label>
-    </div>
-    <div class="flex flex-col items-center justify-start w-full mx-auto">
+        <label for="name">
+            <input type="email" on:input:target=move |ev| {set_email.set(ev.target().value());} />
+        </label>
         <div class="paddle-checkout-box w-full min-h-80"></div>
         <script>{script}</script>
     </div>
